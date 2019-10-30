e3f4fbb7cb5452eeb27e5bfcb5b4349268a0f895
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2019-10-29 21:26:09 -0300

    Added logs

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,4 +1,6 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @moduledoc """
   Module that integrate with Github's API
   """
@@ -6,6 +8,8 @@ defmodule Issues.GithubIssues do
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}'")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response()
@@ -25,6 +29,9 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_error(),
       body |> Poison.Parser.parse!()
